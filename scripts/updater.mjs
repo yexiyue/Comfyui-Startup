a2237f0a39ef388f8895feb1862937e6a8b1feba
yexiyue <yexiyue666@qq.com>
2024-04-16 21:13:08 +0800

    test: ci

--- scripts/updater.mjs
+++ scripts/updater.mjs
@@ -25,17 +25,18 @@ const updateRelease = async () => {
 
   // 上传新的文件
   const file = await readFile("latest.json", { encoding: "utf-8" });
-  const data = JSON.parse(file);
-  if (data.platforms["darwin-x86_64"]) {
-    data.platforms["darwin-aarch64"] = data.platforms["darwin-x86_64"];
-  }
+  // const data = JSON.parse(file);
+  // if (data.platforms["darwin-x86_64"]) {
+  //   data.platforms["darwin-aarch64"] = data.platforms["darwin-x86_64"];
+  // }
 
   await octokit.rest.repos.uploadReleaseAsset({
     owner: context.repo.owner,
     repo: context.repo.repo,
     release_id: release.id,
     name: "latest.json",
-    data: JSON.stringify(data),
+    // data: JSON.stringify(data),
+    data: file,
   });
 };
 
