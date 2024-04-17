2b867aaae22140f572b4803e85c3bb7146bfda5c
yexiyue <yexiyue666@qq.com>
2024-04-17 11:09:38 +0800

    test: publish

--- scripts/publish.mjs
+++ scripts/publish.mjs
@@ -4,24 +4,27 @@ import { join } from "node:path";
 const artifactPaths = JSON.parse(process.env.artifactPaths);
 console.log("artifactPaths", artifactPaths);
 console.log("first", artifactPaths[0]);
-const targetDir = "publish";
-console.log("dir", join(import.meta.dirname, "../", targetDir));
-// const publish = async () => {
-//   await mkdir(targetDir, { recursive: true });
+const targetDir = join(import.meta.dirname, "../", "publish");
 
-//   for await (const artifactPath of artifactPaths) {
-//     const fileName = artifactPath.split("/").pop();
-//     const path = `${targetDir}/${fileName}`;
-//     await copyFile(artifactPath, path);
-//     console.log(`Coping ${artifactPath} ======> ${path}`);
-//   }
+const publish = async () => {
+  await mkdir(targetDir, { recursive: true });
 
-//   const file = await readFile("latest.json", { encoding: "utf-8" });
-//   const data = JSON.parse(file);
-//   if (data.platforms["darwin-x86_64"]) {
-//     data.platforms["darwin-aarch64"] = data.platforms["darwin-x86_64"];
-//   }
-//   await writeFile(`${targetDir}/latest.json`, JSON.stringify(data, null, 2));
-// };
+  for await (const artifactPath of artifactPaths) {
+    const fileName = artifactPath.split("/").pop();
+    const path = join(targetDir, fileName);
+    await copyFile(artifactPath, path);
+    console.log(`Coping ${artifactPath} ======> ${path}`);
+  }
 
-// publish();
+  const file = await readFile("latest.json", { encoding: "utf-8" });
+  const data = JSON.parse(file);
+  if (data.platforms["darwin-x86_64"]) {
+    data.platforms["darwin-aarch64"] = data.platforms["darwin-x86_64"];
+  }
+  await writeFile(
+    join(targetDir, "latest.json"),
+    JSON.stringify(data, null, 2)
+  );
+};
+
+publish();
