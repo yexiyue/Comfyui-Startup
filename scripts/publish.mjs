7d0a8ec97ae1096c5a717dc715afb6d5a89e80fb
yexiyue <yexiyue666@qq.com>
2024-04-17 11:41:28 +0800

    test: publish

--- scripts/publish.mjs
+++ scripts/publish.mjs
@@ -1,20 +1,20 @@
 import { readFile, copyFile, writeFile, mkdir } from "node:fs/promises";
 import { join } from "node:path";
 
-const artifactPaths = JSON.parse(process.env.artifactPaths);
-console.log("artifactPaths", artifactPaths);
-console.log("first", artifactPaths[0]);
+// const artifactPaths = JSON.parse(process.env.artifactPaths);
+// console.log("artifactPaths", artifactPaths);
+// console.log("first", artifactPaths[0]);
 const targetDir = join(import.meta.dirname, "../", "publish");
 
 const publish = async () => {
   await mkdir(targetDir, { recursive: true });
 
-  for await (const artifactPath of artifactPaths) {
-    const fileName = artifactPath.split("/").pop();
-    const path = join(targetDir, fileName);
-    await copyFile(artifactPath, path);
-    console.log(`Coping ${artifactPath} ======> ${path}`);
-  }
+  // for await (const artifactPath of artifactPaths) {
+  //   const fileName = artifactPath.split("/").pop();
+  //   const path = join(targetDir, fileName);
+  //   await copyFile(artifactPath, path);
+  //   console.log(`Coping ${artifactPath} ======> ${path}`);
+  // }
 
   const file = await readFile("latest.json", { encoding: "utf-8" });
   const data = JSON.parse(file);
