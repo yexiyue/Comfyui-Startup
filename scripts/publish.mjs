88c47ea9051511920699888e7a32a9629bf054e9
yexiyue <yexiyue666@qq.com>
2024-04-17 09:56:52 +0800

    test: publish

--- scripts/publish.mjs
+++ scripts/publish.mjs
@@ -8,20 +8,26 @@ const publish = async () => {
   const targetDirPath = new URL(`../${targetDir}`, import.meta.url);
   await mkdir(targetDirPath, { recursive: true });
 
+  // 拷贝文件
   const tasks = artifactPaths.map(async (artifactPath) => {
     const fileName = artifactPath.split("/").pop();
     const path = join(targetDirPath, fileName);
     await copyFile(artifactPath, path);
     console.log(`Coping ${artifactPath} ======> ${fileName}`);
   });
-  Promise.allSettled(tasks);
+
+  await Promise.allSettled(tasks);
 
   const file = await readFile("latest.json", { encoding: "utf-8" });
   const data = JSON.parse(file);
   if (data.platforms["darwin-x86_64"]) {
     data.platforms["darwin-aarch64"] = data.platforms["darwin-x86_64"];
   }
-  await writeFile("publish/latest.json", JSON.stringify(data, null, 2));
+
+  await writeFile(
+    join(targetDirPath, "latest.json"),
+    JSON.stringify(data, null, 2)
+  );
 };
 
 publish();
